d2b46dd5511128cbf9c0a66f6f07a2b587c6c062
Abhishek Pandey <91930405+bird-03@users.noreply.github.com>
2025-10-28 10:13:05 +0530

    Create encryptDecrypt.js

---
+++ Core ServiceNow APIs/GlideRecord/EncryptDecrypt/encryptDecrypt.js
@@ -0,0 +1,9 @@
+/*
+create a folder for your code contribution
+Create a javascript file .js
+readme.md file that explain your code 
+*/
+var myfirstC= new GlideRecord('incident');
+
+
+//THIS IS TEST PLEASE IGNORE
